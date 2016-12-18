e730c0bdeea05d3430065ae5ab5b3fc34665017c
sergio sandoval castillo <sergio.sc120692@gmail.com>
2016-12-17 22:12:17 -0500

    primer commit de la pagina

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Sergio Sandoval on 11/12/2016.
+ */
+edgrid.menu('main-nav','main-menu');
+
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+});
+
+
+$(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+});
+
